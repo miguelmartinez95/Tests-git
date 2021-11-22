db95d2ff1579e189fee956281f60040aadbe865e
miguelmartinez95 <66802481+miguelmartinez95@users.noreply.github.com>
2021-11-22 22:27:31 +0100

    Add files via upload

---
+++ python_practica1.py
@@ -0,0 +1,84 @@
+##########################################################
+#PRÁCTICA 1
+##########################################################
+
+#Ejecución códigos
+######################################
+import numpy as np
+
+print('Hello World')
+
+a= 1
+b= 3
+
+#Vectores
+
+
+#Matrices
+
+
+#Tipos de variables
+######################################
+#Integer
+
+#Números reales (float)
+
+
+#Texto (strings)
+
+
+#Lógico (booleans)
+
+#LIBRERÍAS BÁSICAS
+###################################################
+import tensorflow
+import keras
+import pandas as pd
+
+
+#Importación datos
+######################################
+data = pd.read_csv(r"C:\xxx\xxx\xxx\xxx/xxxx.csv", sep=';', decimal=',')
+data = pd.read_csv('xxxx.csv', sep=';', decimal=',')
+
+#Datos ejemplo
+data = pd.read_csv(r"C:\Users\migue\Documents\Doctorado\Clases\GET\dataset_example.csv", sep=';', decimal=',')
+
+
+
+#Missing values
+######################################
+
+
+#Eliminar filas con NAs
+
+
+#Rellenar huecos con algún valor concreto ('Enmascaramiento')
+
+#Rellenamos huecos con una interpolación
+
+
+#Análisis exploratorio de los datos
+######################################
+
+
+
+#GRÁFICOS
+import matplotlib.pyplot as plt
+
+#Gráfico lineas
+
+
+
+#Histograma
+
+
+#Diagrama de dispersión
+
+
+
+
+
+
+
+
